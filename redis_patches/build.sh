acb86ea0044031e5ee82867dcbb98e03f7d8942e
secwall <secwall@yandex-team.ru>
2024-01-09 13:05:07 +0100

    Bump base redis version to 7.2.4

--- redis_patches/build.sh
+++ redis_patches/build.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install build-essential git
 cd /app
 git clone https://github.com/redis/redis.git
 cd redis
-git checkout 7.2.3
+git checkout 7.2.4
 
 for i in ../redis_patches/*.patch
 do
