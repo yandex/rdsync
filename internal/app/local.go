d229b4042a94762452669f260d94e6e745aab213
secwall <secwall@yandex-team.ru>
2025-09-03 14:31:23 +0200

    Drop health check node from dcs on local node failure

--- internal/app/local.go
+++ internal/app/local.go
@@ -17,15 +17,29 @@ func (app *App) getLocalState() *HostState {
 
 func (app *App) healthChecker() {
 	ticker := time.NewTicker(app.config.HealthCheckInterval)
+	path := dcs.JoinPath(pathHealthPrefix, app.config.Hostname)
+	hcCheckTime := time.Time{}
 	for {
 		select {
 		case <-ticker.C:
 			hc := app.getLocalState()
 			app.logger.Info(fmt.Sprintf("healthcheck: %v", hc))
 			if hc != nil {
-				err := app.dcs.SetEphemeral(dcs.JoinPath(pathHealthPrefix, app.config.Hostname), hc)
+				hcCheckTime = hc.CheckAt
+				err := app.dcs.SetEphemeral(path, hc)
 				if err != nil {
 					app.logger.Error("Failed to set healthcheck status to dcs", "error", err)
+				}
+			} else if !hcCheckTime.IsZero() {
+				if time.Since(hcCheckTime) < 5*app.config.HealthCheckInterval {
+					app.logger.Warn("Unable to get local node state, leaving health node in dcs intact")
+				} else {
+					app.logger.Warn("Unable to get local node state, dropping health node from dcs")
+					err := app.dcs.Delete(path)
+					if err != nil {
+						app.logger.Error("Failed to drop healthcheck status from dcs on dead local node", "error", err)
+					}
+					hcCheckTime = time.Time{}
 				}
 			}
 		case <-app.ctx.Done():
