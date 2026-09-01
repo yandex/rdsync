cb81617b41adaf68d9109220004275ce9eb548ff
secwall <secwall@yandex-team.ru>
2026-09-01 18:18:23 +0200

    Bump valkey to 9.1.2

--- valkey_patches/build.sh
+++ valkey_patches/build.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt -y install build-essential git
 cd /app
 git clone https://github.com/valkey-io/valkey.git
 cd valkey
-git checkout 9.1.1
+git checkout 9.1.2
 
 for i in ../valkey_patches/*.patch
 do
